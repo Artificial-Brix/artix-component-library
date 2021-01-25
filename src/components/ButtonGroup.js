9a19695419b3573f8ca1df260ad8bd4a8b55f433
tamim-hossain <tamim.cloud@gmail.com>
2021-01-25 17:49:15 +0530

    feat: added Button Group

---
+++ src/components/ButtonGroup.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { font, primaryColors, shape } from "/config/styles";
+
+const Button = styled.button`
+  ${font}
+  color: ${(props) => (props.color ? props.color : "#05385b")};
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0.8rem;
+  transition: all ease 0.4s;
+  :not(:last-child) {
+    border-right: 2px solid #05385b;
+  }
+  :hover {
+    opacity: 0.7;
+  }
+`;
+const Wrapper = styled.div`
+  background: ${(props) => (props.background ? props.background : "#5cdb95")};
+  border-radius: ${(props) =>
+    props.borderRadius ? props.borderRadius : "1rem"};
+  border: 1px solid #05385b;
+  display: flex;
+  width: max-content;
+  z-index: 9;
+`;
+
+export default function ButtonGroup(props) {
+  console.log(props);
+  return (
+    <Wrapper {...props}>
+      {props.buttons.map((text, i) => {
+        return <Button {...props}>{text}</Button>;
+      })}
+    </Wrapper>
+  );
+}
---
+++ src/components/ButtonGroup.md
@@ -0,0 +1,15 @@
+A basic Group button
+
+```jsx
+import ButtonGroup from "./ButtonGroup";
+
+<ButtonGroup buttons={["Ironman", "Thor", "Batman"]} />;
+```
+
+using custom props
+
+```jsx
+import ButtonGroup from "./ButtonGroup";
+
+<ButtonGroup buttons={["Ironman", "Thor", "Batman"]} background="#334c66" color="#ffffff" borderRadius="0.8rem" />;
+```
