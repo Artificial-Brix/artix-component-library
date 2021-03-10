2566076dbe2d2dfab89892af3cc9e958c6380e7f
anirudh-99 <61656095+anirudh-99@users.noreply.github.com>
2021-03-10 11:27:31 +0530

    added avatar component (#50)

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const ImageWrapper = styled.img`
+  border-radius: 50%;
+  width: ${(props) => (props.width ? props.width : 50)}px;
+  height: ${(props) => (props.height ? props.height : 50)}px;
+  cursor: pointer;
+`;
+
+export default function (props) {
+  return <ImageWrapper src={props.imgSrc ? props.imgSrc : "/src/assets/defaultAvatar.png"} {...props}></ImageWrapper>;
+}
