66ac2ce8bfa3cfc23905631122a79552e6425cb6
anirudh-99 <anirudhreddybasani@gmail.com>
2021-03-13 19:18:37 +0530

    added list component

---
+++ src/assets/homeIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomeIcon = function(props){
+    return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 32 32"
+          width="24">
+          <path d="M16 1l-16 16 3 3 3-3v13h8v-6h4v6h8v-13l3 3 3-3-16-16zM16 14c-1.105 0-2-0.895-2-2s0.895-2 2-2c1.105 0 2 0.895 2 2s-0.895 2-2 2z"></path>
+     </svg>
+    );
+  };
+
+export default HomeIcon;
