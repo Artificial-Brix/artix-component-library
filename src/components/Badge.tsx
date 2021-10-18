45e3caea7f8a4424df1b0f8caffacb505c9aaab6
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-10-18 16:08:55 +0530

    Created Badge typescript. issue #70

---
+++ src/components/Badge.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { font, primaryColors, shape } from "/config/styles";
+
+const Wrapper = styled.div`
+  position: relative;
+  height: max-content;
+  width: max-content;
+  .badge {
+    position: absolute;
+    ${(props) => (props.bottom ? "top:0" : "bottom: 0")};
+    right: 0;
+    color: ${(props) => (props.color ? props.color : "white")};
+    background: ${(props) => (props.background ? props.background : "red")};
+    z-index: 9;
+    height: 1rem;
+    width: 1rem;
+    padding: 0.1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+  }
+`;
+
+const Square = styled.div`
+  width: 2rem;
+  height: 2rem;
+  background-color: #5cdb95;
+  position: relative;
+`;
+
+export const Badge: React.FC<props> = (props) => {
+  return (
+    <Wrapper {...props}>
+      <p className="badge">{props.val}</p>
+      {props.children}
+    </Wrapper>
+  );
+}
+
+export { Square };
