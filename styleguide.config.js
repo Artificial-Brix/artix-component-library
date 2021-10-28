f6a000f6962414d3f3732a71ad3da3c573efa5e6
Manish Kumar Das <manish.das@krishi.network>
2021-10-29 01:39:20 +0530

    smol changes

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  components: 'src/components/**/*.{js}',
+  title: 'Styleguide',
+  // components: 'src/components/**/*.{js}',
+  components: 'src/components/**/[A-Z]*.js',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+  },
 };
